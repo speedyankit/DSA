e2cc4c8a58f0da38429439e65fee43a5586dc96c
Ankit Kumar Singh <ankitksingh83@gmail.com>
2026-01-22 20:26:19 +0530

    Implement missing number calculation in array

---
+++ DAY 1/missing_in_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNum(self, arr):
+        # code here
+        
+        n = len(arr) + 1
+      
+        total_sum = (n* (n+1))//2
+        arr_sum = sum(arr)
+        
+        return total_sum - arr_sum
